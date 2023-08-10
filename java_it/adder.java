dccf903560ad9ce104bc01ae132f5c71f11ff937
Asibinanwar <95580261+Asibinanwar@users.noreply.github.com>
2023-08-10 22:22:06 +0530

    Update adder.java

--- java_it/adder.java
+++ java_it/adder.java
@@ -1,17 +1,1 @@
-class Adder
-{
-	public static int add(int a, int b)
-	{
-		return a + b;
-	}
 
-	public static int add(int a, int b, int c)
-	{
-		return a + b + c;
-	}
-
-	public static int add(int a, int b, int c, int d)
-	{
-		return a + b + c +d;
-	}
-}
