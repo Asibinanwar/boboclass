1a7f78f904d43d3398b22641389de7b4513400ef
Asibinanwar <95580261+Asibinanwar@users.noreply.github.com>
2023-08-10 22:20:51 +0530

    Create adder.java

---
+++ java_it/method_overloading/adder.java
@@ -0,0 +1,17 @@
+class Adder
+{
+	public static int add(int a, int b)
+	{
+		return a + b;
+	}
+
+	public static int add(int a, int b, int c)
+	{
+		return a + b + c;
+	}
+
+	public static int add(int a, int b, int c, int d)
+	{
+		return a + b + c +d;
+	}
+}
